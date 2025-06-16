c1a4e5744b4eeea324f0a1540d6233d66dfee8d8
AlvesKZ <igoralvex01@gmail.com>
2025-06-15 23:13:31 -0300

    Cria classe Moto

--- src/models/Moto.java
+++ src/models/Moto.java
@@ -1,4 +1,41 @@
 package models;
 
-public class Moto {
+public class Moto extends Veiculo{
+    private boolean partidaEletrica;
+
+    public Moto(String marca, String modelo, int ano, double precoDiaria, boolean alugado, boolean partidaEletrica) {
+        super(marca, modelo, ano, precoDiaria, alugado);
+        setPartidaEletrica(partidaEletrica);
+    }
+
+    public boolean isPartidaEletrica() {
+        return partidaEletrica;
+    }
+
+    public void setPartidaEletrica(boolean partidaEletrica) {
+        this.partidaEletrica = partidaEletrica;
+    }
+
+    @Override
+    public double calcularPrecoAluguel(int dias) {
+        double totalAluguel = getPrecoDiaria() * dias;
+
+        if (dias < 3) {
+            totalAluguel += 15;
+        }
+
+        System.out.printf("Preço total do aluguel: %.2f", totalAluguel);
+        return totalAluguel;
+    }
+
+    @Override
+    public String exibirDetalhes() {
+        return String.format("Veiculo: %s %s (%d) - R$%.2f/dia - Alugado: %s - Partida elétrica: %s",
+                getMarca(), getModelo(), getAno(), getPrecoDiaria(), isAlugado() ? "Sim" : "Não", isPartidaEletrica() ? "Sim" : "Não");
+    }
+
+    @Override
+    public String toString() {
+        return exibirDetalhes();
+    }
 }
