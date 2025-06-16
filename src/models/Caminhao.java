b5b5854e32ae5b95ba2b742135af0e5ddc059b35
AlvesKZ <igoralvex01@gmail.com>
2025-06-15 23:21:39 -0300

    Cria classe Caminhao

--- src/models/Caminhao.java
+++ src/models/Caminhao.java
@@ -1,4 +1,37 @@
 package models;
 
-public class Caminhao {
+public class Caminhao extends Veiculo{
+    private double capacidade;
+
+    public Caminhao(String marca, String modelo, int ano, double precoDiaria, boolean alugado, double capacidade) {
+        super(marca, modelo, ano, precoDiaria, alugado);
+        setCapacidade(capacidade);
+    }
+
+    public double getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(double capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    @Override
+    public double calcularPrecoAluguel(int dias) {
+        double totalAluguel = (getPrecoDiaria() + 50) * dias;
+
+        System.out.printf("Preço total do aluguel: %.2f", totalAluguel);
+        return totalAluguel;
+    }
+
+    @Override
+    public String exibirDetalhes() {
+        return String.format("Veiculo: %s %s (%d) - R$%.2f/dia - Alugado: %s - Capacidade total (em toneladas): %.2f",
+                getMarca(), getModelo(), getAno(), getPrecoDiaria(), isAlugado() ? "Sim" : "Não", getCapacidade());
+    }
+
+    @Override
+    public String toString() {
+        return exibirDetalhes();
+    }
 }
